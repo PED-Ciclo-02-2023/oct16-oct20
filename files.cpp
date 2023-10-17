e21d9bda6dd05c8f478440269150f3df0eeb5716
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-10-17 11:10:36 -0600

    :truck: Move files to new branch

---
+++ files.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm> // Librería para la funcion trim
+#include <cctype>    // Librería para la funcion trim
+#include "lib/functions.hpp"
+
+using namespace std;
+
+// Function to trim leading and trailing whitespace from a string
+string trim(const string& str) {
+    // Se crea una copia de la cadena original.
+    string result = str;
+
+    // Elimina los espacios en blanco iniciales.
+    result.erase(result.begin(), find_if(result.begin(), result.end(), [](int ch) {return !isspace(ch);}));
+
+    // Elimina los espacios en blanco finales.
+    result.erase(find_if(result.rbegin(), result.rend(), [](int ch) {return !isspace(ch);}).base(), result.end());
+
+    /*
+    begin: Devuelve un iterador al primer elemento del contenedor.
+    end: Devuelve un iterador al elemento después del último elemento del contenedor.
+
+    rbegin: Devuelve un iterador al último elemento del contenedor.
+    rend: Devuelve un iterador al elemento antes del primer elemento del contenedor.
+    */
+
+    // Devuelve la cadena resultante sin espacios en blanco iniciales y finales.
+    return result;
+}
+
+// Modify the GetPasswordForUser function
+string GetPasswordForUser(const string& username, const string& filename) {
+    ifstream file(filename);
+    string line;
+    vector<user> users;  // Vector to store users
+
+    if (file.is_open()) {
+        user currentUser;
+        bool isUsername = true;
+
+        while (getline(file, line)) {
+            line = trim(line); // Trim leading and trailing whitespace
+            if (isUsername) {
+                currentUser.username = line;
+            } else {
+                currentUser.password = line;
+                users.push_back(currentUser);  // Add the current user to the vector
+            }
+            isUsername = !isUsername;
+        }
+        file.close();
+
+        for (auto user : users) {
+            if (user.username == username) {
+                return user.password; // Return the password if the username is found
+            }
+        }
+
+        return "User not found";  // Return a message if the username is not found
+    } else {
+        return "File not found"; // Return a message if the file cannot be opened
+    }
+}
---
+++ lib/structs.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct user {
+    string username;
+    string password;
+};
+
+struct dish {
+    string name;
+    float price;
+};
+
+struct drink {
+    string name;
+    float price;
+};
+
+struct apt { // Appetizer
+    string name;
+    float price;
+};
+
+struct order {
+    string client;
+    int ID;
+    stack<dish> dishes;
+    stack<drink> drinks;
+    stack<apt> apts;
+    bool is_card_payment;
+    bool is_delivery;
+}; typedef struct order Order;
